a09972b4af059bef50216da5f109a00c69faad06
Jinsil117 <jinsil117@nate.com>
2018-12-18 17:44:25 +0900

    add  lookbook

--- app/src/main/java/com/pickth/dddd/smartcoordination/MainActivity.java
+++ app/src/main/java/com/pickth/dddd/smartcoordination/MainActivity.java
@@ -1,6 +1,8 @@
 package com.pickth.dddd.smartcoordination;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
@@ -9,8 +11,12 @@ import android.support.v7.widget.RecyclerView;
 import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
+import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TabHost;
 import android.widget.Toast;
+
+import java.io.InputStream;
 
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener{
@@ -21,6 +27,9 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     private Boolean isFabOpen = false;
     private FloatingActionButton fab, fab1, fab2;
     private final int GALLERY_CODE=1112;
+
+    ImageView imageView; //갤러리에서 선택한 이미지를 보여주기 위한 것
+    Button albumbtn; //xml에서 btn_album과 연결하기 위한 것
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -49,6 +58,23 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         fab.setOnClickListener(this);
         fab1.setOnClickListener(this);
         fab2.setOnClickListener(this);
+
+
+        //룩북 탭
+        imageView = (ImageView)findViewById(R.id.iv_view);
+
+        albumbtn = (Button) findViewById(R.id.btn_album);
+        albumbtn.setOnClickListener(new View.OnClickListener() { //갤러리 버튼 클릭 시 갤러리로 접근
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(); //이미지 불러오기
+                intent.setType("image/*");
+                intent.setAction(Intent.ACTION_GET_CONTENT);
+                intent.putExtra(Intent.EXTRA_ALLOW_MULTIPLE, true); //갤러리에서 여러 이미지를 선택
+                startActivityForResult(intent, 1); //시작할 액티비티를 통해 어떠한 값을 받을 것을 기대하고 액티비티를 시작
+            }
+        });
+
     }
 
     public void setTabHost() {
@@ -114,6 +140,23 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             fab1.setClickable(true);
             fab2.setClickable(true);
             isFabOpen = true;
+        }
+    }
+    protected  void onActivityResult(int requestCode, int resultCode, Intent data) { //startActivityForResult를 실행했을 때, 받기를 기대했던 어떠한 값 동작을 정의하는 메소드
+        if (requestCode == 1) {
+            // Make sure the request was successful
+            if (resultCode == RESULT_OK) {
+                try {
+                    // 선택한 이미지에서 비트맵 생성
+                    InputStream in = getContentResolver().openInputStream(data.getData());
+                    Bitmap img = BitmapFactory.decodeStream(in);
+                    in.close();
+                    // 이미지 표시
+                    imageView.setImageBitmap(img);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
         }
     }
 }
